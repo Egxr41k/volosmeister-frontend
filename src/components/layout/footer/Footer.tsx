4e81843d1b485bec4f52326ae950a8c1c726a71d
Egxr41k <egor2005krava@gmail.com>
2025-07-11 10:20:29 +0000

    updating footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,9 +1,12 @@
 const Footer = () => {
 	return (
-		<footer className="">
-			<p className="pt-5 text-center text-sm text-black">
-				<a href="https://github.com/Egxr41k">© Egxr41k</a> | volosmeister
-			</p>
+		<footer className="pt-5 text-center text-sm text-black">
+			<p>Сайт здався незручним?</p>
+			<a className="underline" href="https://www.instagram.com/volosmeister/">
+				Розкажіть нам про це
+			</a>
+
+			{/* <a href="https://github.com/Egxr41k">© Egxr41k</a> */}
 		</footer>
 	)
 }
