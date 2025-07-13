b4c6e9df01f55038cc82743db4de275c7560d01b
Egxr41k <70434378+Egxr41k@users.noreply.github.com>
2025-07-13 12:39:53 +0300

    Update Footer.tsx

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
 	return (
 		<footer className="pt-5 text-center text-sm text-black">
-			<p>Сайт здався незручним?</p>
-			<a className="underline" href="https://www.instagram.com/volosmeister/">
+			<p>Сайт здався незручним?
+			<a className="underline ml-2" href="https://www.instagram.com/volosmeister/">
 				Розкажіть нам про це
 			</a>
-
+                        </p>
 			{/* <a href="https://github.com/Egxr41k">© Egxr41k</a> */}
 		</footer>
 	)
